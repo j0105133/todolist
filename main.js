e6823c7e3150c954c28f744195fd2e10b0b479c0
stephanie <j0102276@gmail.com>
2024-07-05 13:55:14 +0900

    final commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let textInput = document.getElementById("text-input");
 let addBtn = document.getElementById("add-btn");
 let tabs = document.querySelectorAll(".task-tabs div");
 let underLine = document.getElementById("under-line");
-let filterList=[];
+let filterList = [];
 let taskList = [];
 let mode = "all";
 let heartBurst = document.getElementById("heart-burst");
@@ -17,7 +17,6 @@ addBtn.addEventListener("click", function(event) {
     addTask();
     createHearts(event);
 });
-
 
 function createHearts(event) {
     let rect = addBtn.getBoundingClientRect();
@@ -43,24 +42,19 @@ function createHearts(event) {
     }
 }
 
-
-for(let i=1; i<tabs.length;i++){
+for(let i=1; i<tabs.length; i++){
     tabs[i].addEventListener("click", function(event){
-            filter(event);
-            moveUnderLine(event.target);
-        });
-    }
+        filter(event);
+        moveUnderLine(event.target);
+    });
+}
 
-
-// 밑줄 이동 함수 수정
 function moveUnderLine(clickedTab) {
     underLine.style.left = clickedTab.offsetLeft + "px";
     underLine.style.width = clickedTab.offsetWidth + "px";
 }
 
-
 function addTask() {
-
     if (textInput.value.trim() === "") {
         alert("할일을 입력해주세요!");
         return;
@@ -69,21 +63,14 @@ function addTask() {
         id: randomId(),
         taskContent: textInput.value,
         isComplete: false,
-    }
-        taskList.push(task);
-        textInput.value = ""; // 입력 필드를 비웁니다.
-        render();
-    
+    };
+    taskList.push(task);
+    textInput.value = ""; // 입력 필드를 비웁니다.
+    updateFilteredTasks();
 }
 
 function render() {
-    let list =[];
-    if(mode ==="all"){
-        list = taskList;
-    }else if(mode ==="not-done" || mode ==="done" ){
-        list = filterList;
-    }
-
+    let list = mode === "all" ? taskList : filterList;
 
     let resultHTML = "";
 
@@ -119,54 +106,26 @@ function toggleComplete(id) {
         }
     }
     updateFilteredTasks();
-
 }
 
-
-function updateFilteredTasks(){
-    // 우리가 보고 싶어하는 목록만 골라서 화면에 다시 보여줘요.
+function updateFilteredTasks() {
     filterList = taskList.filter(task => {
-        if(mode === "all") return true;
+        if (mode === "all") return true;
         return mode === "done" ? task.isComplete : !task.isComplete;
     });
     render();
 }
+
 function deleteTask(id) {
-    for(let i =0; i<taskList.length; i++){
-        if(taskList[i].id == id){
-            taskList.splice(i,1);
-             break;
-      }
-    }
-    render();
+    taskList = taskList.filter(task => task.id !== id);
+    updateFilteredTasks();
 }
-function filter(event){
-    console.log(filter)
-    filterList = [];
-    mode =event.target.id;
-    
-    if( mode ==="all"){
-        render();
-    }else if(mode ==="not-done"){
-        for(let i =0; i<taskList.length;i++){
-            if(taskList[i].isComplete === false){
-                filterList.push(taskList[i]);
-            
-            }
-        }
-       render();
-    }else if(mode ==="done"){
-        for(let i =0; i<taskList.length;i++){
-            if(taskList[i].isComplete === true){
-                filterList.push(taskList[i]);
-            
-            }
-        }
-       render();
-    }
+
+function filter(event) {
+    mode = event.target.id;
+    updateFilteredTasks();
 }
 
 function randomId() {
     return '_' + Math.random().toString(36).substring(2, 9);
-   
 }
